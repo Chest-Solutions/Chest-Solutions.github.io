341cf436493d005991017fa505b45697d2718386
RedSnicker (@Absurd_y) <115105740+Economic1024@users.noreply.github.com>
2024-07-10 21:28:15 +0330

    Create loadmenu.js

---
+++ Dev/js/loadmenu.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', () => {
+    const loadingScreen = document.getElementById('loadingScreen');
+    const mainContent = document.getElementById('mainContent');
+  
+    // Set a timeout to gradually hide the loading screen
+    setTimeout(() => {
+      loadingScreen.style.opacity = 0; // Set opacity to 0
+      setTimeout(() => {
+        loadingScreen.style.display = 'none'; // Hide the loading screen after the transition
+      }, 1000); // Wait for the transition duration (in milliseconds)
+      
+      // Show main content
+      mainContent.style.display = 'block';
+    }, 2000); // Delay before starting the transition
+  });
