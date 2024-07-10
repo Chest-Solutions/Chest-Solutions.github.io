1efa181eba60c0e8452b9ebc3d7ada71b93790b3
anmvc <anmvced@gmail.com>
2024-07-10 08:38:19 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ function displayMainContent() {
     var mainContent = document.createElement('div');
     
     // Apply a CSS class to increase the text size
-    mainContent.classList.add('medium-text');
+    mainContent.classList.add('small-text');
 
     // Set the text content
     mainContent.textContent = "Chest Solutions"; // Example content - replace this with your actual website content
