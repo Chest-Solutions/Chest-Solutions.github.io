94d27fd4b84d289d4c85f192e8e43404eca5e8fe
Muf4ldWasHere <mrdragondragon54@gmail.com>
2024-07-19 21:31:06 +0200

    Update stars.js

--- js/stars.js
+++ js/stars.js
@@ -3,23 +3,16 @@ import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.132.2/examples/jsm/l
 
 let model, clonedModel;
 
-
-// Set up the scene
 const scene = new THREE.Scene();
-
-// Create a camera
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 0, 5);
 
-// Create a renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Create a star field
 const starGeometry = new THREE.BufferGeometry();
 const starMaterial = new THREE.PointsMaterial({ color: 0xffffff });
-
 starMaterial.renderOrder = -5;
 
 const starVertices = [];
@@ -34,23 +27,16 @@ starGeometry.setAttribute('position', new THREE.Float32BufferAttribute(starVerti
 const stars = new THREE.Points(starGeometry, starMaterial);
 scene.add(stars);
 
-// Create a basic ambient light
 const ambientLight = new THREE.AmbientLight(0x404040);
 scene.add(ambientLight);
-const loader = new GLTFLoader();
 
+const loader = new GLTFLoader();
 loader.load(
   'models/chest.glb',
   function (gltf) {
     model = gltf.scene;
-
-    model.traverse((child) => {
-        //hello!
-    });
-    
+    model.traverse((child) => {});
     model.renderOrder = 1;
-
-    // Clone the model
     clonedModel = model.clone();
     scene.add(clonedModel);
     scene.add(model);
@@ -63,22 +49,16 @@ loader.load(
 
 const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
 scene.add(light);
-camera.position.z = 500
 
-
-
-// Animation loop
 function animate() {
   requestAnimationFrame(animate);
-
   stars.rotation.x += 0.0005;
   stars.rotation.y += 0.0005;
   if (model) {
-    model.rotation.y += 0.02
+    model.rotation.y += 0.02;
     model.rotation.x += 0.05;
-    model.rotation.z += 0.01
+    model.rotation.z += 0.01;
   }
-
   renderer.render(scene, camera);
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
