616a823c447ec72affb7f8f0dced63d8c2f52366
RedSnicker <179263069+RedSnicker@users.noreply.github.com>
2026-07-17 01:16:20 +0330

    Update SmoothScroll.jsx

--- src/components/SmoothScroll.jsx
+++ src/components/SmoothScroll.jsx
@@ -5,13 +5,13 @@ import Lenis from '@studio-freight/lenis';
 const SmoothScroll = ({ children }) => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
+      duration: 0.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
+      wheelMultiplier: 0.1,
+      touchMultiplier: 0.2,
     });
 
     function raf(time) {
